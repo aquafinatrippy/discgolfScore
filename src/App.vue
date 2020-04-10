4d77f7ab9836aca931e0ca33fd5d8bdc041011ad
Tanel Tekko <taneldeviis@hotmail.com>
2020-04-10 23:54:15 +0300

    dialog workin

--- src/App.vue
+++ src/App.vue
@@ -2,54 +2,53 @@
   <v-app>
     <v-container>
       <div class="text-center">
-          <v-btn class="mx-2" fab dark large color="purple" :click="dialog == true">
+        <v-btn class="mx-2" fab dark large color="purple" @click="dialog = true">
+          <v-icon dark>mdi-account-multiple-plus</v-icon>
+        </v-btn>
+      </div>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card color="#385F73" dark>
+            <v-card-title class="headline">Unlimited music now</v-card-title>
 
-    
-      <v-icon dark>mdi-android</v-icon>
-    </v-btn>
-  </div>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
 
-  <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Privacy Policy
-        </v-card-title>
+            <v-card-actions>
+              <v-btn text>Listen Now</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
 
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
+      <v-dialog v-model="dialog" width="500">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>Privacy Policy</v-card-title>
 
-        <v-divider></v-divider>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     dialog: false
   }),
+  methods: {
+    addPerson() {
+      this.dialog = true;
+    }
+  }
 };
 </script>
