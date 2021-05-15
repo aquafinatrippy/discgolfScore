aebae402437e7b1ed3743b4c35d79330b67111d3
Tanel Tekko <taneldeviis@hotmail.com>
2021-05-16 01:09:13 +0300

    New start screen view

---
+++ src/views/Start.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+         <v-card max-width="600">
+          <v-card-title class="headline grey lighten-2" primary-title
+            >Vali rada või loo enda rada</v-card-title>
+
+          <v-card-text>
+              <strong>RAJA loomine </strong>
+              <p>Vali mitu korvi on ja alusta skoori lugemist.</p>
+
+             <v-btn>Loo rada</v-btn> <br>
+               <strong>Raja valimine </strong>
+              <p>Vali ette lisatud rada kus mängida sooviksid ja alusta soori lugemist.</p>
+
+             <v-btn>VALI RADA</v-btn> <br>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+        </v-card>
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    name: "Start"
+}
+</script>
