4450d844fc6adb3a3ddc3b0b1157454ab010f9c0
Tanel Tekko <taneldeviis@hotmail.com>
2021-05-16 15:05:49 +0300

    start view changes

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,21 +1,26 @@
 <template>
     <v-container class="startCont">
-         <v-card max-width="600">
-          <v-card-title class="headline grey lighten-2" primary-title
+         <v-card max-width="600" max-height="600">
+          <v-card-title class="headline warning lighten-2" primary-title
             >Vali rada või loo enda rada</v-card-title>
 
-          <v-card-text>
-              <strong>RAJA loomine </strong>
-              <p>Vali mitu korvi on ja alusta skoori lugemist.</p>
+          <v-card-text style="height: 100%;">
+              <div class="cardContent">
+                <strong>RAJA loomine </strong>
+                <p>Vali mitu korvi on ja alusta skoori lugemist.</p>
 
-             <v-btn>Loo rada</v-btn> <br>
-               <strong>Raja valimine </strong>
-              <p>Vali ette lisatud rada kus mängida sooviksid ja alusta soori lugemist.</p>
+                <v-btn color="primary">Loo rada</v-btn> <br>
+                <v-divider></v-divider>
 
-             <v-btn>VALI RADA</v-btn> <br>
+                <strong>Raja valimine </strong>
+                <p>Vali ette lisatud rada kus mängida sooviksid ja alusta soori lugemist.</p>
+
+                <v-btn color="primary">VALI RADA</v-btn> <br>
+              </div>
+             
           </v-card-text>
 
-          <v-divider></v-divider>
+
 
         </v-card>
     </v-container>
@@ -33,5 +38,11 @@ export default {
    display: flex;
    justify-content: center;
    height: 100%;
+}
+.cardContent{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
 }
 </style>
