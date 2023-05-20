ab072cb98d194580f8c115e054892dca0c0dcbf8
aquafinatrippy <tanel.tekko@gmail.com>
2023-05-20 10:03:29 +0300

    Added basket counting feature

---
+++ src/components/TrackingBaskets.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pa-4 trackCounter">
+    <p class="text-bold">Mitmes rada:</p>
+    <div class="d-inline-flex">
+      <v-btn x-large class="mx-2" color="error" @click="basket -= 1">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <h1>{{ basket }}</h1>
+      <v-btn x-large class="mx-2" color="primary" @click="basket += 1">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrackBaskets",
+  data() {
+    return {
+      basket: 0,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.trackCounter {
+  width: 100%;
+  justify-content: center;
+  margin-top: 30px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,63 @@
 <template>
-
   <v-app class="mt-10">
-
     <Navbar>
-
       <v-btn class="mx-2" fab dark large color="success" @click="dialog = true">
-
         <v-icon dark>mdi-account-multiple-plus</v-icon>
-
       </v-btn>
-
     </Navbar>
 
     <v-container>
-
+      <TrackingBaskets />
       <PlayerList :players="players" />
 
       <v-dialog v-model="dialog" width="500">
-
         <v-card>
-
           <v-card-title class="headline grey lighten-2" primary-title>
-             Lisa mängija
+            Lisa mängija
           </v-card-title>
 
           <v-card-text>
-
             <v-text-field v-model="playerName" label="Nimi"></v-text-field>
-
           </v-card-text>
 
           <v-divider></v-divider>
 
           <v-card-actions>
-
             <v-spacer></v-spacer>
 
             <v-btn color="primary" text @click="addPlayer()">Lisa</v-btn>
 
             <v-btn color="danger" text @click="dialog = false">sulge</v-btn>
-
           </v-card-actions>
-
         </v-card>
-
       </v-dialog>
-
     </v-container>
-
   </v-app>
-
 </template>
 
 <script>
- import Navbar from "@/components/Navbar"; import PlayerList from "@/components/PlayerList";
-export default { name: "Home", components: { Navbar, PlayerList, }, data: () => ({
-dialog: false, players: [], playerName: "", infoDialog: false, }), methods: { addPlayer()
-{ let players = this.players; let player = { name: this.playerName, score: 0, };
-players.push(player); this.dialog = false; }, }, };
+import Navbar from "@/components/Navbar";
+import PlayerList from "@/components/PlayerList";
+import TrackingBaskets from "@/components/TrackingBaskets";
+
+export default {
+  name: "Home",
+  components: { Navbar, PlayerList, TrackingBaskets },
+  data: () => ({
+    dialog: false,
+    players: [],
+    playerName: "",
+    infoDialog: false,
+  }),
+  methods: {
+    addPlayer() {
+      let players = this.players;
+      let player = { name: this.playerName, score: 0 };
+      players.push(player);
+      this.dialog = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -69,4 +69,3 @@ players.push(player); this.dialog = false; }, }, };
   }
 }
 </style>
-
